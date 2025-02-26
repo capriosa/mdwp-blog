4da621de966a4e0bf4116da4a4c8a17ced2ece87
Meinolf Droste <mdwp.post@gmail.com>
2025-02-26 16:48:33 +0100

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,11 +1,10 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Meinolf Droste';
+    : 'mdwp';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'mdwp Blog 
-       about No-Code and AI';
+    : 'Blog about No-Code and AI';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
